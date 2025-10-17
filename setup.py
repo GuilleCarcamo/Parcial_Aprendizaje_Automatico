261c7737adfda817c146cb2024c34c7b79db57dc
GuilleCarcamo <carcamo.guille.26@gmail.com>
2025-10-17 20:48:54 -0300

    Estructura creada

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Proyecto de Aprendizaje Automatico para el diagnostico de endometiosis',
+    author='Guillermo Carcamo',
+    license='MIT',
+)
